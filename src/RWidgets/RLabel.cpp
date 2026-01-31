78b755b55a1e20e24fe083db73b0f2b90bf5f99c
SemkiShow <mgdeveloper123@gmail.com>
2026-01-31 16:53:17 +0100

    Fix multi-line labels centering

--- src/RWidgets/RLabel.cpp
+++ src/RWidgets/RLabel.cpp
@@ -11,7 +11,36 @@ void RLabel::Shrink()
     SetSize(size);
 }
 
-void RLabel::Draw() { rui::DrawText(font, label, GetPosition(), maxSize.y, fontSpacing, color); }
+void RLabel::Draw()
+{
+    auto lines = Split(label, '\n');
+    RVector2 size = rui::MeasureText(font, label, maxSize.y, fontSpacing);
+    float posY = GetPositionY();
+    for (auto& line: lines)
+    {
+        auto lineSize = rui::MeasureText(font, line, maxSize.y, fontSpacing);
+        float posX = GetPositionX();
+
+        switch (alignH)
+        {
+        case RAlign::Left:
+            break;
+        case RAlign::HCenter:
+            posX += (size.x - lineSize.x) / 2;
+            break;
+        case RAlign::Right:
+            posX += (size.x - lineSize.x);
+            break;
+        default:
+            throw std::runtime_error("Invalid alignH set!");
+            break;
+        }
+
+        rui::DrawText(font, line, {posX, posY}, maxSize.y, fontSpacing, color);
+
+        posY += lineSize.y;
+    }
+}
 
 void RLabel::UpdateLabels()
 {
